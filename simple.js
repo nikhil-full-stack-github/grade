d42752678356472cc09619cf5762bd60683ae52c
Nikhil P <134950904+nikhil-full-stack-github@users.noreply.github.com>
2023-07-02 13:59:11 +0530

    nikhil

---
+++ simple.js
@@ -0,0 +1,61 @@
+let a=15
+let b=20
+let result=a+b
+console.log(result)
+
+     //
+
+let c=20
+let d=30
+let answer=c-d
+console.log(c-d)
+
+       //
+       var e=80
+       var f=90
+       var y=e*f
+       console.log(y)      
+      
+             //
+      
+        var g=70
+        var h=40
+        var x=g/h
+        console.log(x)
+        
+             //
+      
+       const i=600
+       const j=500
+       const k=i+j
+       console.log(k)
+
+           //
+
+       const m=1000
+       const n=2000
+       const l=m*n
+       console.log(l)
+       
+           //
+      const o=4000
+      const p=5000
+      const q=p/o
+      console.log(q)
+
+
+          //
+
+     const r=50
+     const s=60
+     const t=70
+     const u=80
+     const v=r*s+t/u     
+     console.log(v)
+
+           //
+
+     const w=70
+     const z=100
+     const rst=w%z
+     console.log(rst)      
