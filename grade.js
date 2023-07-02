e7867cccbaf63b2178d13b6274c3f08c18ad52b1
unknown <nikhilppry2001@gmail.com>
2023-07-02 13:37:46 +0530

    nikhil

---
+++ grade.js
@@ -0,0 +1,17 @@
+let a=51
+
+if(a>90 && a<=100 ){
+    console.log("mark =A")
+}
+else if(a>80 && a<=90){
+    console.log("mark =B")
+}
+  else if(a>70 && a<=80){
+        console.log("mark =C")
+   }
+else if(a>60 && a<=70){
+    console.log("mark=D")
+   }
+ else if(a>50 && a<=60){
+    console.log("mark=E failed")
+   }
